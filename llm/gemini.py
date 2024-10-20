5a2c10972920436aad2eebe5a990da45d642bc65
Amit Thakur <hello@amitthakur.org>
2024-10-19 22:44:12 -0700

    Added Gemini

--- llm/gemini.py
+++ llm/gemini.py
@@ -4,11 +4,19 @@ GOOGLE_API_KEY = 'AIzaSyA22TDRXukEEihXBe7d3xcqNp2pBdTxsbE'
 genai.configure(api_key=GOOGLE_API_KEY)
 prompt_file = '/Users/amitthakur/PycharmProjects/BookChat/prompts/conversation-1.txt'
 
+def query_prompt(query_lines):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    chat = model.start_chat(history=[])
+    # lines = read_prompt(prompt_file)
+    response = chat.send_message(query_lines)
+    return response.text
+
 def read_prompt(file_path):
     with open(file_path, 'r') as file:
         # Read all lines into a list
         lines = file.readlines()
         return lines
+
 def main():
     model = genai.GenerativeModel('gemini-1.5-flash')
     chat = model.start_chat(history=[])
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, File, UploadFile
 import shutil
 from epub_parser.epub_to_text import epub_to_chapters, get_chapter
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Union
+from llm.gemini import query_prompt
 
 app = FastAPI()
 
@@ -19,6 +22,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+class LLMQuery(BaseModel):
+    query: Union[str, None] = None
+
 
 
 @app.get("/")
@@ -45,4 +53,13 @@ async def read_chapter(bookname: str, ch_no: int):
         "ch_no": ch_no,
         "content": content
     }
+
+@app.post("/llm-query")
+async def llm_query(llm_query: LLMQuery):
+    llm_response = query_prompt(llm_query.query)
+    return {
+        "response": llm_response
+    }
+
+
 
